b9dfbdcd014b26c136be64ac67eb08de2eaed76a
constantine <okonemmanuelmma@gmail.com>
2024-12-16 14:45:18 +0100

    Initial commit

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <img />
+        <div>Enver</div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
