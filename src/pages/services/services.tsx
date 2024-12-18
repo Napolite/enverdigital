5dc9d0d6d27006487596b2105c39ed598850b778
constantine <okonemmanuelmma@gmail.com>
2024-12-18 22:25:53 +0100

    I dunno

---
+++ src/pages/services/services.tsx
@@ -0,0 +1,21 @@
+function Services() {
+  return (
+    <div>
+      <div>
+        <img />
+        <div>The Service We Provide For You</div>
+        <img />
+      </div>
+      <div>
+        {/* <img src={} />
+        <img src={} />
+        <img src={} />
+        <img src={} />
+        <img src={} />
+        <img src={} /> */}
+      </div>
+    </div>
+  );
+}
+
+export default Services;
