acf594dea7d4f509e5672924a1626844872f01b4
constantine <okonemmanuelmma@gmail.com>
2025-02-07 21:35:26 +0100

    some animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Footer from "./components/footer/footer";
 import Navbar from "./components/navbar/navbar";
@@ -9,36 +10,46 @@ import Services from "./pages/services/services";
 import VideoPage from "./pages/video/videoPage";
 
 function App() {
-  // const [pa]
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPage(2);
+    }, 5800);
+  }, []);
   return (
     <>
-      <div>
-        <Loader />
-      </div>
+      {page === 1 && (
+        <div>
+          <Loader />
+        </div>
+      )}
       <div className="h-[3px] fixed top-0 z-999 bg-[#5454d4] w-full scroll-watcher"></div>
-      {/* <div>
-        <div className="px-[200px]">
-          <Navbar />
+      {page === 2 && (
+        <div>
+          <div className="px-[200px]">
+            <Navbar />
+          </div>
+          <div className="px-[200px]">
+            <Landing />
+          </div>
+          <div className="px-[200px]">
+            <VideoPage />
+          </div>
+          <div className="px-[200px]">
+            <Services />
+          </div>
+          <div className="px-[200px]">
+            <Portfolio />
+          </div>
+          <div className="">
+            <Contact />
+          </div>
+          <div className="px-[200px] mt-[50px]">
+            <Footer />
+          </div>
         </div>
-        <div className="px-[200px]">
-          <Landing />
-        </div>
-        <div className="px-[200px]">
-          <VideoPage />
-        </div>
-        <div className="px-[200px]">
-          <Services />
-        </div>
-        <div className="px-[200px]">
-          <Portfolio />
-        </div>
-        <div className="">
-          <Contact />
-        </div>
-        <div className="px-[200px] mt-[50px]">
-          <Footer />
-        </div>
-      </div> */}
+      )}
     </>
   );
 }
--- src/pages/loading/load.tsx
+++ src/pages/loading/load.tsx
@@ -1,16 +1,30 @@
+import { useEffect, useState } from "react";
 import { LogoIcon } from "../../assets";
 import "./loader.css";
 
 function Loader() {
+  const [switchAnim, setSwitch] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSwitch(true);
+    }, 4000);
+  }, []);
   return (
     <div className="text-white">
       <div className="flex justify-center items-center content-center -translate-y-[50px]  h-screen">
         <img
           src={LogoIcon}
-          className="h-[100px] w-[100px] mr-[20px] loader-logo"
+          className={`h-[100px] w-[100px] mr-[20px] ${
+            switchAnim ? "loader-logo-leave" : "loader-logo"
+          }`}
         />
         <div>
-          <div className="text-[60px] font-[Graphik] leading-20 tracking-normal font-[700] logoFade">
+          <div
+            className={`text-[60px] font-[Graphik] leading-20 tracking-normal font-[700] ${
+              switchAnim ? "logoFade-leave" : "logoFade"
+            }`}
+          >
             Enver Digital Agency
           </div>
           {/* <div className="underline"></div> */}
